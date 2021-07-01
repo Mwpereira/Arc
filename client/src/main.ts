790acce4da970b9d1dfa440727a63a2f52db1cc6
Michael Pereira <michael.w.pereira.7@gmail.com>
2021-07-01 12:26:54 -0400

    Updating main.ts

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
-
 import './registerServiceWorker';
 import router from './router/router';
+import './providers/vee-validate';
 
 import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
-import './providers/vee-validate';
+
 import './assets/css/styles.scss';
 
 Vue.use(Buefy);
